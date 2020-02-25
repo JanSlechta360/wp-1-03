5ab6b4fee3a6e91fb61c8171faa7808850788566
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-02-25 08:58:34 +0100

    Update index.php
    Přidán index a šest proměnných

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-      $a=10
+      $a=10;
       $b=5
 ?>
       <head>
@@ -10,13 +10,13 @@
     <title>Document</title>
 </head>
 <body>
-     <p> A= <?= $a ?>
-     <p> B= <?= $b ?>
+     <p> A = <?= $a ?> </p>
+     <p> B = <?= $b ?> </p>
 
      <p>Součet <?= $a ?> + <?= $b ?> = <?= $a + $b ?> </p>
      <p>Rozdíl <?= $a ?> - <?= $b ?> = <?= $a - $b ?> </p>
-     <p>Součin <?= $a ?> x <?= $b ?> = <?= $a x $b ?> </p>
+     <p>Součin <?= $a ?> * <?= $b ?> = <?= $a * $b ?> </p>
      <p>Podíl  <?= $a ?> / <?= $b ?> = <?= $a / $b ?> </p> 
-     
+
 </body>
 </html>
