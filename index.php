4397ab58a0f4ce35d24ac9be3f09fbe8e7213d5c
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-03-03 08:35:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
           $portion = $a / $b;
       } else {
           $portion = "nelze dělit nulou"
-?>]
+?>
       <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
