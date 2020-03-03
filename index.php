0077abe318a76d12e45b46d28af22e0551fae012
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-03-03 08:25:11 +0100

    pridani podminky

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
       $a=10;
       $b=5;
-?>
+
+      $sumarry = $a + $b;
+      $difference = $a - $b;
+      $product = $a * $b;
+
+      if ($b != 0) {
+          $portion = $a / $b;
+      } else {
+          $portion = "nelze dělit nulou"
+?>]
       <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,10 +23,10 @@
      <p> A = <?= $a ?> </p>
      <p> B = <?= $b ?> </p>
 
-     <p>Součet <?= $a ?> + <?= $b ?> = <?= $a + $b ?> </p>
-     <p>Rozdíl <?= $a ?> - <?= $b ?> = <?= $a - $b ?> </p>
-     <p>Součin <?= $a ?> * <?= $b ?> = <?= $a * $b ?> </p>
-     <p>Podíl  <?= $a ?> / <?= $b ?> = <?= $a / $b ?> </p> 
+     <p>Součet <?= $a ?> + <?= $b ?> = <?= $sumarry ?> </p>
+     <p>Rozdíl <?= $a ?> - <?= $b ?> = <?= $difference ?> </p>
+     <p>Součin <?= $a ?> * <?= $b ?> = <?= $product ?> </p>
+     <p>Podíl  <?= $a ?> / <?= $b ?> = <?= $portion ?> </p> 
 
 </body>
 </html>
